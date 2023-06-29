4d6fc8dbd537fbf4c25edf325ba1b8a769c165b9
criggins44 <128842735+criggins44@users.noreply.github.com>
2023-06-28 21:28:53 -0400

    added comments to explain logic in the code

--- script.js
+++ script.js
@@ -6,24 +6,18 @@ Write a JavaScript function to calculate the factorial of a number.
 
 
 function factorial(num) {
+
   if (num === 0){
     return 1;
+/*
+makes sure that there is a value capable of being multiplied by increasing the
+    minimum value to 1
+*/
   }
 
   return num * factorial(num - 1);
+//multiplies the number by the all the numbers that come before it
+  
 }
 
 console.log(factorial(4));
-
-
-/*
-function factorial(num) { 
-  if (num === 0){
-    return 1;
-  }
-  
-  return num * factorial(num - 1);       
-}
-
-console.log(factorial(3));
-*/
